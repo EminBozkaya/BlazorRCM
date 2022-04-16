4940fc6efe6840e9db5fc180bebd850e285d5bbf
MyLaptop <152wingman@gmail.com>
2022-04-16 18:37:36 +0300

    QuickSale Page Design

---
+++ BlazorRCM/Shared/DTOs/ViewDTOs/QuickSaleBillDTO.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlazorRCM.Shared.DTOs.ViewDTOs
+{
+    public class QuickSaleBillDTO
+    {
+        public short LineNo { get; set; }
+        public bool HasNote { get; set; }
+        public string? ProductName { get; set; }
+        public float Portion { get; set; }
+        public float ProductPrice { get; set; }
+        public short Quantity { get; set; }
+        public float TotalPrice { get; set; }
+        
+    }
+}
